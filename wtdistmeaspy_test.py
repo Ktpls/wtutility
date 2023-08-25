912f17825dc70ab07b7340daa3375a0b1d1a1826
Kita <kita@live.cn>
2023-08-25 22:14:51 +0800

    wtdmp new sightLineZero helping detecting better

--- wtdistmeaspy_test.py
+++
@@ -1,9 +0,0 @@
-from wtdistmeaspy import *
-def testground():
-    scr=cv.imread(
-    r"C:\prog\wtutility\asset\wtdistmeaspy\log\2023-08-24-18-23-04_NormalTrace\unnamed.png")
-    ret=SolveMap_BottomRightSmallMap(scr,dbg=True,dbglogpath=r'./asset/wtdistmeaspy/log/{}/'.format(
-        time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime())
-        ))
-    print(ret)
-testground()
