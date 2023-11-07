ae89b95881ea85af415cfcf1184b14d6225bda45
Kita <ktdpls@gmail.com>
2023-11-07 18:07:21 +0800

    -

--- utilitypack/util_windows.py
+++ utilitypack/util_windows.py
@@ -471,3 +471,43 @@ class HotkeyManager:
                     traceback.print_exc()
                 if throwonerr:
                     raise e
+
+
+"""
+xls
+"""
+
+import openpyxl as opx
+
+
+def save_list_to_xls(data_list, filename, sheetname=None):
+    # Create a new workbook
+    wb = opx.Workbook()
+
+    # Select the active worksheet
+    if sheetname is None:
+        ws = wb.active
+    else:
+        ws = wb.create_sheet(sheetname)
+
+    # Iterate over the list and write each item to a new row
+    for row, item in enumerate(data_list):
+        ws.cell(row=row + 1, column=1, value=item)
+
+    # Save the workbook to the specified filename
+    wb.save(filename)
+
+
+def Xls2ListList(path=None, sheetname=None, killNones=True):
+    if path is None:
+        path = r"eles.in.xlsx"
+    xls = opx.load_workbook(path)
+    if sheetname is None:
+        sheet = xls.active
+    else:
+        sheet = xls[sheetname]
+
+    ret = [[ele.value for ele in ln] for ln in (sheet.rows)]
+    if killNones:
+        ret = [l for l in ret if any([e is not None for e in l])]
+    return ret
