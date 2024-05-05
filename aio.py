0af45459bc753f25fbe0d8d752003e75ed4167eb
Kita <kita@live.cn>
2024-05-05 23:27:36 +0800

    -

--- aio.py
+++ aio.py
@@ -1,33 +1,41 @@
-from concurrent.futures import ThreadPoolExecutor
-import keyshortcut.gameinput as gameinput
-from utilitypack.utility import *
 from utilitypack.util_app import *
 from aio_config import *
-import functools
-import keyshortcut.keyshortcut as keyshortcut
 import shared.globalsys as globalsys
+from engineman import engineman
+from glock import glock
+from keyshortcut import keyshortcut
+from telescope import telescope
+from wtdmp import wtdistmeaspy
 
 
 def main():
     app = BulletinApp(fps=aiofps, config=HotKeyConfig)
+    modules: list[globalsys.WtUtilityModule] = list()
 
     # wtdistmeas
     if usingwtdistmeaspy:
         print("wtdistmeaspy activated")
+        modules.append(wtdistmeaspy.mWtdmp())
 
     # telescope
     if usingtelescope:
         print("telescope activated")
+        modules.append(telescope.mTelescope())
 
     # key shortcuts
     if usingkeyshortcut:
         print("keyshortcut activated")
+        modules.append(keyshortcut.mKeyshortcut())
 
     if usingglock:
         print("glock activated")
+        modules.append(glock.mGlock())
 
     if usingengineman:
         print("engineman activated")
+        modules.append(engineman.mEngineman())
+    for m in modules:
+        m.load()
 
     @app.Hotkey("Reboot", app.config.HotKey_Reboot)
     def rebootfoo():
---
+++ exp/ongoing/newStoppableThread.py
@@ -0,0 +1,127 @@
+from utilref import *
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import time
+
+
+class MyThread(object):
+    def __init__(self):
+        self.stop_event = threading.Event()
+
+    def run(self):
+        while not self.stop_event.is_set():
+            # Do some work here
+            print("Working...")
+            time.sleep(1)
+
+    def stop(self):
+        self.stop_event.set()
+
+
+class StoppableThreadState(enum.Enum):
+    stopped = 1
+    running = 2
+    stopping = 3
+
+
+class StoppableThread2(StoppableSomewhat):
+    """
+    derivate from it and override foo()
+    """
+
+    # TODO give option to handle error by user. thats for wtutily system to log error
+    def __init__(
+        self,
+        strategy_runonrunning: "StoppableSomewhat.StrategyRunOnRunning" = None,
+        strategy_error: "StoppableSomewhat.StrategyError" = None,
+        pool: ThreadPoolExecutor = None,
+    ) -> None:
+        super().__init__(strategy_runonrunning, strategy_error)
+        self.stopsignal = threading.Event()
+        self.pool: ThreadPoolExecutor = (
+            pool if pool is not None else ThreadPoolExecutor()
+        )
+        self.future = None
+        self.state = StoppableThreadState.stopped
+
+    def foo(self, *arg, **kw) -> typing.Any:
+        raise NotImplementedError("should never run without overriding foo")
+
+    def isRunning(self) -> bool:
+        return self.state == StoppableThreadState.running
+
+    @FunctionalWrapper
+    def go(self, *arg, **kw) -> None:
+        if self.isRunning():
+            if (
+                self.strategy_runonrunning
+                == StoppableThread.StrategyRunOnRunning.raise_error
+            ):
+                raise RuntimeError("already running")
+            elif (
+                self.strategy_runonrunning
+                == StoppableThread.StrategyRunOnRunning.stop_and_rerun
+            ):
+                self.stop()
+            elif (
+                self.strategy_runonrunning
+                == StoppableThread.StrategyRunOnRunning.skip_and_return
+            ):
+                return
+        self.ensureStopped()
+        self.stopsignal.clear()
+
+        def call() -> None:
+            """
+            wrapper so can call the passed "self"'s foo
+            if not, can never know which overwritten foo should be called
+            check if self.stopsignal when any place to break
+            """
+            ret=None
+            try:
+                ret= self.foo(*arg, **kw)
+            except Exception as e:
+                if self.strategy_error == StoppableThread.StrategyError.raise_error:
+                    raise e
+                elif self.strategy_error == StoppableThread.StrategyError.print_error:
+                    traceback.print_exc()
+                elif self.strategy_error == StoppableThread.StrategyError.ignore:
+                    pass
+            self.state=StoppableThreadState.stopped
+            self.stopsignal.clear()
+            return ret
+
+        self.future = self.pool.submit(call)
+        self.state = StoppableThreadState.running
+
+    @FunctionalWrapper
+    def stop(self) -> None:
+        # add critical section here
+        if self.state in (StoppableThreadState.stopped, StoppableThreadState.stopping):
+            return
+        self.state = StoppableThreadState.stopping
+        self.stopsignal.set()
+
+    def ensureStopped(self):
+        if self.future is not None:
+            self.future.result()
+        self.state=StoppableThreadState.stopped
+
+    def timeToStop(self) -> bool:
+        return self.stopsignal.is_set()
+
+
+def main():
+    t = MyThread()
+    with ThreadPoolExecutor() as executor:
+        future = executor.submit(t.run)
+        # Do some other work here
+        time.sleep(5)
+        # Stop the thread
+        t.stop()
+        # Wait for the thread to finish
+        future.result()
+
+
+if __name__ == "__main__":
+    main()
