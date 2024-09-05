f8680c9b11edcbe38ce5dc56e7ca02dd39b525ba
Kita <ktdpls@gmail.com>
2024-09-05 15:03:48 +0800

    move aenum util out from util_solid

--- afm/autofreshmap_implementation.py
+++ afm/autofreshmap_implementation.py
@@ -414,6 +414,25 @@ def leaveButton():
     moveto([0, 0])
 
 
+import aenum
+
+
+def ExtendEnum(src):
+    def deco_inner(cls):
+        nonlocal src
+        if (
+            issubclass(src, aenum.Enum)
+            or aenum.stdlib_enums
+            and issubclass(src, aenum.stdlib_enums)
+        ):
+            src = src.__members__.items()
+        for name, value in src:
+            aenum.extend_enum(cls, name, value)
+        return cls
+
+    return deco_inner
+
+
 class MessagedThread:
     class MessageType(aenum.Enum):
         stop = 0
