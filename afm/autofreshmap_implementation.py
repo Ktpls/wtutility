ab6b98577a27ac1b724848361d35f8f4d7b36d35
Kita <ktdpls@gmail.com>
2025-12-21 14:05:53 +0800

    move file

--- afm/autofreshmap_implementation.py
+++ afm/autofreshmap_implementation.py
@@ -3,8 +3,8 @@ from utilitypack.cold.util_solid import LongDelay
 from utilitypack.util_winkey import *
 from shared.globalsys import *
 import matplotlib.pyplot as plt
-from .autofreshmap_configmap_importref import *
-from .autofreshmap_config import *
+from .config.autofreshmap_configmap_importref import *
+from .config.autofreshmap_config import *
 
 MapSize = [648, 648]
 
